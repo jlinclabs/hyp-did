ae66429489ff9714a99ac9a483148cd019fad7ac
Jared Grippe <jared@deadlyicon.com>
2022-04-22 14:30:11 -0700

    WIP: refactored cli

--- cli/hyp-did.js
+++ cli/hyp-did.js
@@ -2,32 +2,104 @@
 
 process.title = "hyp-did"
 
+import Path from 'path'
+import operatingSystem from 'os'
 import subcommand from 'subcommand'
 import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import chalk from 'chalk'
-
-import { commands } from './index.js'
+import { DidClient } from 'hyp-did'
 
 const packageJson = JSON.parse(fs.readFileSync(path.join(fileURLToPath(import.meta.url), '../../package.json'), 'utf8'))
 
+export const commands = {
+  create: {
+    name: 'create',
+    description: 'Create a new did',
+    usage: {
+      simple: 'Create a new did',
+      full: 'Create a new did and hypercore'
+    },
+    async command({ didClient }){
+      await didClient.ready()
+      const didDocument = await didClient.create()
+      console.log('created', didDocument)
+    }
+  },
+  resolve: {
+    name: 'resolve',
+    description: 'get the did document for the given did',
+    usage: {
+      simple: 'get did document',
+      full: 'get the did document for the given did',
+    },
+    async command(args){
+      console.log('resolve', args)
+    }
+  },
+  superseed: {
+    name: 'superseed',
+    description: 'superseed',
+    usage: {
+      simple: 'superseed',
+      full: 'superseed',
+    },
+    async command(args){
+      console.log('superseed', args)
+    }
+  },
+  revoke: {
+    name: 'revoke',
+    description: 'revoke',
+    usage: {
+      simple: 'revoke',
+      full: 'revoke',
+    },
+    async command(args){
+      console.log('revoke', args)
+    },
+  },
+  history: {
+    name: 'history',
+    description: 'get the history for the given did',
+    usage: {
+      simple: 'get the history for the given did',
+      full: 'get the history for the given did'
+    },
+    command(args){
+      console.log('history', args)
+    },
+  },
+}
+
+Object.values(commands).forEach(wrapCommand)
+
 // match & run the command
-subcommand({ commands: Object.values(commands).map(wrapCommand), none })(process.argv.slice(2))
+subcommand({ commands: Object.values(commands), none })(process.argv.slice(2))
 
 function wrapCommand(cmd){
-  return {
-    ...cmd,
-    async command(...args){
-      if (args[0].h || args[0].help) {
-        usage(cmd)
-        process.exit(0)
-      }
-      try{
-        return await cmd.command(...args)
-      }catch(error){
-        fail(error)
-      }
+  const { command } = cmd
+  cmd.command = async function(args){
+    if (args.v || args.verbose) args.verbose = true
+    if (args.h || args.help) {
+      usage(cmd)
+      process.exit(0)
+    }
+
+    let storagePath = Path.join(operatingSystem.homedir(), '.hyp-did', 'storage')
+    if (args.verbose){
+      console.error(`${chalk.gray(`storing hypercores in ${storagePath}`)}`)
+    }
+
+    args.didClient = new DidClient({
+      storagePath
+    })
+
+    try{
+      return await command(args)
+    }catch(error){
+      fail(error)
     }
   }
 }
@@ -45,7 +117,8 @@ function none(args){
 }
 
 function fail(error) {
-  console.error(chalk.red(`${error}\n`))
+  console.error(error)
+  // console.error(chalk.red(`${error}\n`))
   process.exit(1)
 }
 
