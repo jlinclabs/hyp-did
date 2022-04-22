fef4d4abfd8cca877c8747abe1d3dfeef6fca4c4
Jared Grippe <jared@deadlyicon.com>
2022-04-22 14:30:25 -0700

    WIP: more dev on core functions

--- DidDocument.js
+++ DidDocument.js
@@ -1,36 +1,63 @@
 
-import { keyToDid } from './util.js'
+import { keyToString, didToKey } from './util.js'
 
 export default class DidDocument {
-  constructor(core){
+  constructor(did, core){
     this.did = did
     this.publicKey = didToKey(did)
     this.core = core
-    this.writable = this.core.writable
+    console.log(this)
   }
+
+  get writable(){ return this.core.writable }
 
   [Symbol.for('nodejs.util.inspect.custom')](depth, opts){
     let indent = ''
     if (typeof opts.indentationLvl === 'number')
       while (indent.length < opts.indentationLvl) indent += ' '
     return this.constructor.name + '(\n' +
-      indent + '  id: ' + opts.stylize(this.id, 'string') + '\n' +
+      indent + '  did: ' + opts.stylize(this.did, 'string') + '\n' +
       indent + '  writable: ' + opts.stylize(this.writable, 'boolean') + '\n' +
+      indent + '  publicKey: ' + opts.stylize(this.publicKey, 'string') + '\n' +
       indent + ')'
   }
 
   async update(){
-    await this.ready()
+    // await this.ready()
+    console.log('CORE!', this.core)
     await this.core.update()
+    console.log('CORE!', this.core)
+    if (keyToString(this.core.key) !== this.publicKey)
+      throw new Error(`key mismatch ${[keyToString(this.core.key), this.publicKey]}`)
+    this._ready = true
+    const json = await this.core.get(this.core.length - 1)
+    this._value = JSON.parse(json)
+  }
+
+  get value(){
+    if (this._ready) return this._value
+    throw new Error(`cannot get value before ready`)
   }
 
   async exists(){
-    // TODO maybe try/catch timeout?
-    // await this.update()
-    // return !!this.               xvalue
+    await this.update()
+    return this.core.length > 0
   }
+
+
 
   async create(){
-
+    await this.update()
+    // write to it
+    // ¿ ensure it was replicated to a permanode ?
+      // we could curl a did-server and invoke their caching of it
   }
+
+
+  async _append(newValue){
+    newValue = { ...newValue, updatedAt: new Date }
+    const json = JSON.stringify(newValue)
+    return await this.core.append([json])
+  }
+
 }
--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 
-// import { inspect } from 'util.js'
+import { keyPair } from 'hypercore-crypto'
 import HypercoreClient from './HypercoreClient.js'
 import { keyToString, keyToBuffer, keyToDid, didToKey } from './util.js'
+import DidDocument from './DidDocument.js'
 
 // TODO change this to a derivation of "hyp-did"
 const TOPIC_KEY = keyToBuffer('604d03ea2045c1adfcb6adad02d71667e03c27ec846fe4f5c4d912c10464aea0')
 
-export default class HypDid extends HypercoreClient {
+
+export class DidClient extends HypercoreClient {
+
+  [Symbol.for('nodejs.util.inspect.custom')](depth, opts){
+    let indent = ''
+    if (typeof opts.indentationLvl === 'number')
+      while (indent.length < opts.indentationLvl) indent += ' '
+    return this.constructor.name + '(\n' +
+      indent + '  id: ' + opts.stylize(this.id, 'string') + '\n' +
+      indent + '  writable: ' + opts.stylize(this.writable, 'boolean') + '\n' +
+      indent + ')'
+  }
+
   async create(){
     await this.ready()
-    let { publicKey, secretKey } = crypto.keyPair()
-    const did = keyToDid(publicKey)
-    const didDocument = new DidDocument(this, did, secretKey)
+    let { publicKey, secretKey } = keyPair()
+    const core = await this._getCore(publicKey, secretKey)
+    const didDocument = new DidDocument(keyToDid(publicKey), core)
     await didDocument.create()
     return didDocument
   }
 
+  async _getCore(key, secretKey){
+    return await this.corestore.get({ key, secretKey })
+  }
+
   async get(did, secretKey){
     // const publicKey = didToPublicKey(did)
-    const core = this.corestore.get({
-      key: didToKey(did),
-      secretKey,
-    })
-    const didDocument = new DidDocument(core)
+    const core = await this._getCore(didToKey(did), secretKey)
+    const didDocument = new DidDocument(did, core)
     if (await didDocument.exists()) return didDocument
   }
 }
+
+export { keyPair }
