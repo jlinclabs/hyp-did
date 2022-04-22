575337e0fd58cb6d7c98ca253526e7c1ac057194
Jared Grippe <jared@deadlyicon.com>
2022-04-22 12:50:56 -0700

    WIP: added basic commands to cli

--- cli/hyp-did.js
+++ cli/hyp-did.js
@@ -13,43 +13,60 @@ import { commands } from './index.js'
 const packageJson = JSON.parse(fs.readFileSync(path.join(fileURLToPath(import.meta.url), '../../package.json'), 'utf8'))
 
 // match & run the command
-subcommand({
-  commands: [
+subcommand({ commands: Object.values(commands).map(wrapCommand), none })(process.argv.slice(2))
 
-  ],
-  none(args){
-    if (args.version){
-      console.log(packageJson.version)
-      process.exit(0)
-    }
-    if (args._[0]){
-      console.error(`Invalid command: ${args._[0]}`)
-    }else{
-      usage()
+function wrapCommand(cmd){
+  return {
+    ...cmd,
+    async command(...args){
+      if (args[0].h || args[0].help) {
+        usage(cmd)
+        process.exit(0)
+      }
+      try{
+        return await cmd.command(...args)
+      }catch(error){
+        fail(error)
+      }
     }
   }
-})(process.argv.slice(2))
+}
 
-
-export default function usage (commands, err, cmd) {
-  if (err) {
-    console.error(chalk.red(`${err}\n`))
-  } else {
-    console.error('')
+function none(args){
+  if (args.version){
+    console.log(packageJson.version)
+    process.exit(0)
   }
+  if (args._[0]){
+    console.error(`Invalid command: ${args._[0]}`)
+  }else{
+    usage()
+  }
+}
 
+function fail(error) {
+  console.error(chalk.red(`${error}\n`))
+  process.exit(1)
+}
+
+function usage(cmd){
   if (cmd) {
     console.error(simple(cmd))
     if (cmd.usage.full) console.error(cmd.usage.full)
     else console.error('')
-    process.exit(err ? 1 : 0)
+    return
   }
 
   console.error(`Usage: ${chalk.bold(process.title)} <command> ${chalk.gray(`[opts...]`)}
 
+  ${simple(commands.create)}
+  ${simple(commands.resolve)}
+  ${simple(commands.superseed)}
+  ${simple(commands.revoke)}
+  ${simple(commands.history)}
+
   ${chalk.green(`Learn more at ${packageJson.homepage}`)}
 `)
-  process.exit(err ? 1 : 0)
 }
 
 function simple (cmd) {
--- cli/index.js
+++ cli/index.js
@@ -1,7 +1,59 @@
 #!/usr/bin/env node
 
-process.title = "hyp-did"
-
 export const commands = {
-
+  create: {
+    name: 'create',
+    description: 'Create a new did',
+    usage: {
+      simple: 'Create a new did',
+      full: 'Create a new did and hypercore'
+    },
+    command(args){
+      console.log('create', args)
+    }
+  },
+  resolve: {
+    name: 'resolve',
+    description: 'get the did document for the given did',
+    usage: {
+      simple: 'get did document',
+      full: 'get the did document for the given did',
+    },
+    command(args){
+      console.log('resolve', args)
+    }
+  },
+  superseed: {
+    name: 'superseed',
+    description: 'superseed',
+    usage: {
+      simple: 'superseed',
+      full: 'superseed',
+    },
+    command(args){
+      console.log('superseed', args)
+    }
+  },
+  revoke: {
+    name: 'revoke',
+    description: 'revoke',
+    usage: {
+      simple: 'revoke',
+      full: 'revoke',
+    },
+    command(args){
+      console.log('revoke', args)
+    },
+  },
+  history: {
+    name: 'history',
+    description: 'get the history for the given did',
+    usage: {
+      simple: 'get the history for the given did',
+      full: 'get the history for the given did'
+    },
+    command(args){
+      console.log('history', args)
+    },
+  },
 }
