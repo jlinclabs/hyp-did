7175a2afc59057ac255cf41918bec127d1d9ad8e
Jared Grippe <jared@deadlyicon.com>
2022-05-04 11:21:20 -0700

    WIP

--- util.spec.js
+++ util.spec.js
@@ -13,26 +13,9 @@ function test(){
   const keyAsString = keyToString(keyAsBuffer)
   const keyAsBuffer2 = keyToBuffer(keyAsString)
   const keyAsString2 = keyToString(keyAsBuffer2)
-  console.assert(
-    keyAsString === keyAsString2,
-    `expected "${keyAsString}" === "${keyAsString2}"`
-  )
-  // process.stdout.write('.')
+  if (keyAsString !== keyAsString2)
+    throw new Error(`expected "${keyAsString}" === "${keyAsString2}"`)
 }
 
 let count = 1000 * 10
 while (count--) test()
-
-
-
-// const keyAsB64 = '7HD8ACcToEtnDYNFuQ38c3UuglDirVAlRrN-C_ALB65'
-// const keyAsBuffer = keyToBuffer(keyAsB64)
-
-// console.assert(
-//   keyToString(keyAsBuffer) === keyAsB64,
-//   `expected\n  "${keyToString(keyAsBuffer)}"\n  ===\n  "${keyAsB64}"`,
-// )
-
-
-
-// // THIS IS FUCKED!~!
