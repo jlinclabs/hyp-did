c7c98a707f491e14d1f9498de96492b41ac01661
Jared Grippe <jared@deadlyicon.com>
2022-05-11 16:21:42 -0700

    moved all nested npm modules to other repositories

--- index.js
+++
@@ -1,9 +0,0 @@
-import JlinxServer from 'jlinx-core/JlinxServer.js'
-import JlinxClient from 'jlinx-core/JlinxClient.js'
-import DidDocument from 'jlinx-core/DidDocument.js'
-
-export {
-  JlinxServer,
-  JlinxClient,
-  DidDocument,
-}
