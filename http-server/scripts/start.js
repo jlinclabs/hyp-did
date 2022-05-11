55fe98da327baf77528571f9eac66fe80f64367e
Jared Grippe <jared@deadlyicon.com>
2022-05-11 09:54:53 -0700

    WIP

--- http-server/scripts/start.js
+++ http-server/scripts/start.js
@@ -1,19 +1,7 @@
 #!/usr/bin/env node
 
-import hypDidServer from '../index.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const optionsSpec = {
-  port: ['PORT'],
-  jlinxStoragePath: ['JLINX_STORAGE'],
-}
-const options = {}
-for (const [option, [envVar]] of Object.entries(optionsSpec)){
-  const value = process.env[envVar]
-  if (!value) throw new Error(`environment variable ${envVar} is missing`)
-  options[option] = value
-}
-
-console.log(options)
-
-const server = hypDidServer(options)
-server.start()
+import jlinxHttpServer from '../index.js'
+jlinxHttpServer.start()
