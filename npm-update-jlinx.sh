53e74e42714f92d3058f2671a4f82a627ecaf373
Jared Grippe <jared@deadlyicon.com>
2022-05-25 16:13:02 -0700

    removed git commit from npm-update-jlinx.sh

--- npm-update-jlinx.sh
+++ npm-update-jlinx.sh
@@ -3,8 +3,6 @@
 set -e
 set -x
 
-PACKAGES=$(ls -1 node_modules | grep jlinx-)
+PACKAGES=$(echo $(ls -1 node_modules | grep jlinx-))
 npm upgrade $PACKAGES
-git diff package.json package-lock.json
-git add package.json package-lock.json
-git ci -m "upgraded npm dep $PACKAGES"
+git diff package.json
