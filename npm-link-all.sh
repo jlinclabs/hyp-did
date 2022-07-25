cd8b94691d394f50b455507a0d12269088ea743d
Jared Grippe <jared@deadlyicon.com>
2022-07-25 11:59:31 -0700

    WIP

--- npm-link-all.sh
+++ npm-link-all.sh
@@ -7,7 +7,7 @@ set -x
 
 (cd node && npm link ../util && ls -la ./node_modules/jlinx-*);
 (cd host && npm link ../util ../vault ../node && ls -la ./node_modules/jlinx-*);
-(cd client && npm link ../util ../vault && ls -la ./node_modules/jlinx-*);
-(cd cli && npm link ../util ../client ../vault && ls -la ./node_modules/jlinx-*);
-(cd demo-apps && npm link ../util ../client ../vault && ls -la ./node_modules/jlinx-*);
-(cd desktop/release/app && npm link ../../../util ../../../client ../../../vault ../../../identification  && ls -la ./node_modules/jlinx-*);
+(cd client && npm link ../util ../vault ../host && ls -la ./node_modules/jlinx-*);
+# (cd cli && npm link ../util ../client ../vault && ls -la ./node_modules/jlinx-*);
+# (cd demo-apps && npm link ../util ../client ../vault && ls -la ./node_modules/jlinx-*);
+# (cd desktop/release/app && npm link ../../../util ../../../client ../../../vault ../../../identification  && ls -la ./node_modules/jlinx-*);
